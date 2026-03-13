49a5e43814f855560202c38765eaa4d85c07cc16
zlfn <ung@zlfn.space>
2026-03-13 18:13:45 +0900

    [Z80] Move runtime library to compiler-rt
    Move Z80/SM83 runtime sources from llvm/lib/Target/Z80/runtime/ to
    compiler-rt/lib/builtins/{z80,sm83}/ following standard LLVM
    conventions. Register Z80/SM83 architectures in builtin-config-ix.cmake.
    Update the Z80 backend CMakeLists.txt to reference compiler-rt sources
    via relative path. Add .init section to linker script for entry point
    placement.

--- compiler-rt/cmake/builtin-config-ix.cmake
+++ compiler-rt/cmake/builtin-config-ix.cmake
@@ -99,6 +99,8 @@ set(WASM32 wasm32)
 set(WASM64 wasm64)
 set(VE ve)
 set(M68K m68k)
+set(Z80 z80)
+set(SM83 sm83)
 
 if(APPLE)
   set(ARM64 arm64 arm64e)
@@ -110,7 +112,8 @@ set(ALL_BUILTIN_SUPPORTED_ARCH
   ${X86} ${X86_64} ${AMDGPU} ${ARM32} ${ARM64} ${AVR}
   ${HEXAGON} ${MIPS32} ${MIPS64} ${NVPTX} ${PPC32} ${PPC64}
   ${RISCV32} ${RISCV64} ${S390X} ${SPARC} ${SPARCV9}
-  ${WASM32} ${WASM64} ${VE} ${LOONGARCH64} ${M68K})
+  ${WASM32} ${WASM64} ${VE} ${LOONGARCH64} ${M68K}
+  ${Z80} ${SM83})
 
 include(CompilerRTUtils)
 include(CompilerRTDarwinUtils)
